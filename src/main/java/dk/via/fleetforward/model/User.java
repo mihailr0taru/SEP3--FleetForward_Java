4b51e12f998a4c631661c4d9c943545e061c8518
Carolina Valencia Zavidei <zavidei.carolina@gmail.com>
2025-11-12 15:08:58 +0100

    User Entity Java Finalized

--- src/main/java/dk/via/fleetforward/model/User.java
+++ src/main/java/dk/via/fleetforward/model/User.java
@@ -1,48 +1,85 @@
 package dk.via.fleetforward.model;
 
-import dk.via.fleetforward.utility.StringUtility;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 /**
  * User entity class for storing user information
- * @implNote This class is mapped to the database table '...'
- * @see StringUtility
+ * @implNote This class is mapped to the database table 'app_user'
  */
 @Entity
 @Table(name = "app_user", schema = "fleetforward")
 
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+    @Column(name = "first_name", unique = true, nullable = false)
     String firstName;
+    @Column(name = "last_name", nullable = false)
     String lastName;
+    @Column(name = "email", nullable = false)
     String email;
+    @Column(name = "phone_number", unique = true, nullable = false)
     String phoneNumber;
+    @Column(name = "hashed_password", nullable = false)
     String password;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    UserRole role;
 
-    /**
-     * Get the id of the user
-     * @return The id
-     */
     public Integer getId() {
         return id;
     }
 
-    /**
-     * Set the id of the user
-     * @param id The id to set
-     */
     public void setId(Integer id) {
         this.id = id;
     }
 
-    /**
-     * Get the first name of the user
-     * @return The first name
-     */
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
     }
 }
